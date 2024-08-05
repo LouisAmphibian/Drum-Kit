3b1b51a187ebb5ab4740d9ce45f052d13db369b0
LouisAmphibian <ayandamantshinga30@gmail.com>
2024-08-05 20:10:45 +0200

    Added sound when clicked

---
+++ index.js
@@ -0,0 +1,48 @@
+var numberOfDrumButton = document.querySelectorAll(".drum").length;  //To get number of drum button
+
+//to access every button and changing each button index
+for (var i =0; i<numberOfDrumButton; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+
+        //to get the property in the inner Html
+        var buttonInnerHTML = this.innerHTML;    
+
+        //cases for each inner htm key
+        switch(buttonInnerHTML){
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            //asdjkl
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+             case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break; 
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+            case "k":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            case "l":
+                var kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+
+            default: console.log(buttonInnerHTML);
+        }
+    });
+}
+
+
